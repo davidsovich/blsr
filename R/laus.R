0de52f0ba9d21a89b684123826f4b68d37bc9cf3
David Sovich <davidsovich@gmail.com>
2021-02-01 14:36:04 -0600

    February 1 commits

--- R/laus.R
+++ R/laus.R
@@ -37,7 +37,7 @@ laus_download = function(bls_key, start_year, end_year, adjustment, states, data
 
 #' Download state unemployment rates from the Local Area Unemployment Statistics (LAUS) database
 #'
-#' \code{laus_download} downloads state unemployment rates from the Local Area Unemployment
+#' \code{laus_urate} downloads state unemployment rates from the Local Area Unemployment
 #' Statistics (LAUS) database.
 #'
 #' This function downloads and cleans pre-packaged state unemployment rate data from the LAUS
@@ -55,7 +55,7 @@ laus_download = function(bls_key, start_year, end_year, adjustment, states, data
 #' @param adjustment Character vector. Seasonal adjustment ("S") or not ("U") or both.
 #' @return Cleaned data frame of LAUS data.
 #' @examples
-#' laus_urate(Sys.getenv("BLS_KEY"), "nfp", 2010, 2015, "U")
+#' laus_urate(Sys.getenv("BLS_KEY"), "all", 2010, 2015, "U")
 laus_urate = function(bls_key, states = "all", start_year, end_year, adjustment) {
    if(states == "all") {
       states = laus_codes_list$state_codes %>%
@@ -81,7 +81,143 @@ laus_urate = function(bls_key, states = "all", start_year, end_year, adjustment)
    )
 }
 
+#' Download state unemployment numbers from the Local Area Unemployment Statistics (LAUS) database
+#'
+#' \code{laus_unemp} downloads state unemployment numbers from the Local Area Unemployment
+#' Statistics (LAUS) database.
+#'
+#' This function downloads and cleans pre-packaged state unemployment levels data from the LAUS
+#' database. The default setting is to download unemployment levels for all states in the U.S.
+#' excluding D.C. The user can shorten or lengthen the list of states but is constrained by the
+#' BLS 50 series limit.
+#'
+#' @export
+#'
+#' @param bls_key BLS API key for the user. See vignette.
+#' #' @param states Character vector. All states ("all"), Washington D.C. ("DC"), or list of state
+#' codes. See vignette and \code{laus_codes_list} object.
+#' @param start_year Year to start data download.
+#' @param end_year Year to end data download.
+#' @param adjustment Character vector. Seasonal adjustment ("S") or not ("U") or both.
+#' @return Cleaned data frame of LAUS data.
+#' @examples
+#' laus_unemp(Sys.getenv("BLS_KEY"), "all", 2010, 2015, "U")
+laus_unemp = function(bls_key, states = "all", start_year, end_year, adjustment) {
+   if(states == "all") {
+      states = laus_codes_list$state_codes %>%
+         dplyr::filter(
+            in_us_flag == 1,
+            state_id != "DC"
+         )
+      states = states$state_code
+   } else if (states == "DC") {
+      states = laus_codes_list$state_codes %>%
+         dplyr::filter(
+            state_id == "DC"
+         )
+      states = states$state_code
+   }
+   laus_download(
+      bls_key = bls_key,
+      start_year = start_year,
+      end_year = end_year,
+      adjustment = adjustment,
+      states = states,
+      data_types = c("04")
+   )
+}
 
+#' Download state empoyment numbers from the Local Area Unemployment Statistics (LAUS) database
+#'
+#' \code{laus_emp} downloads state empoyment numbers from the Local Area Unemployment
+#' Statistics (LAUS) database.
+#'
+#' This function downloads and cleans pre-packaged state empoyment levels data from the LAUS
+#' database. The default setting is to download empoyment levels for all states in the U.S.
+#' excluding D.C. The user can shorten or lengthen the list of states but is constrained by the
+#' BLS 50 series limit.
+#'
+#' @export
+#'
+#' @param bls_key BLS API key for the user. See vignette.
+#' #' @param states Character vector. All states ("all"), Washington D.C. ("DC"), or list of state
+#' codes. See vignette and \code{laus_codes_list} object.
+#' @param start_year Year to start data download.
+#' @param end_year Year to end data download.
+#' @param adjustment Character vector. Seasonal adjustment ("S") or not ("U") or both.
+#' @return Cleaned data frame of LAUS data.
+#' @examples
+#' laus_emp(Sys.getenv("BLS_KEY"), "all", 2010, 2015, "U")
+laus_emp = function(bls_key, states = "all", start_year, end_year, adjustment) {
+   if(states == "all") {
+      states = laus_codes_list$state_codes %>%
+         dplyr::filter(
+            in_us_flag == 1,
+            state_id != "DC"
+         )
+      states = states$state_code
+   } else if (states == "DC") {
+      states = laus_codes_list$state_codes %>%
+         dplyr::filter(
+            state_id == "DC"
+         )
+      states = states$state_code
+   }
+   laus_download(
+      bls_key = bls_key,
+      start_year = start_year,
+      end_year = end_year,
+      adjustment = adjustment,
+      states = states,
+      data_types = c("05")
+   )
+}
+
+#' Download state labor force numbers from the Local Area Unemployment Statistics (LAUS) database
+#'
+#' \code{laus_labor_force} downloads state labor force numbers from the Local Area Unemployment
+#' Statistics (LAUS) database.
+#'
+#' This function downloads and cleans pre-packaged state labor force levels data from the LAUS
+#' database. The default setting is to download labor force levels for all states in the U.S.
+#' excluding D.C. The user can shorten or lengthen the list of states but is constrained by the
+#' BLS 50 series limit.
+#'
+#' @export
+#'
+#' @param bls_key BLS API key for the user. See vignette.
+#' #' @param states Character vector. All states ("all"), Washington D.C. ("DC"), or list of state
+#' codes. See vignette and \code{laus_codes_list} object.
+#' @param start_year Year to start data download.
+#' @param end_year Year to end data download.
+#' @param adjustment Character vector. Seasonal adjustment ("S") or not ("U") or both.
+#' @return Cleaned data frame of LAUS data.
+#' @examples
+#' laus_labor_force(Sys.getenv("BLS_KEY"), "all", 2010, 2015, "U")
+laus_labor_force = function(bls_key, states = "all", start_year, end_year, adjustment) {
+   if(states == "all") {
+      states = laus_codes_list$state_codes %>%
+         dplyr::filter(
+            in_us_flag == 1,
+            state_id != "DC"
+         )
+      states = states$state_code
+   } else if (states == "DC") {
+      states = laus_codes_list$state_codes %>%
+         dplyr::filter(
+            state_id == "DC"
+         )
+      states = states$state_code
+   }
+   laus_download(
+      bls_key = bls_key,
+      start_year = start_year,
+      end_year = end_year,
+      adjustment = adjustment,
+      states = states,
+      data_types = c("06")
+   )
+}
 
 
 
